df880ee309b70de5b98f357b328c76119fdfadf9
John Carter <johncarter352@gmail.com>
2020-03-29 01:29:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,4 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase = 
+  if phrase = phrase.upcase
+    "NO, NOT SINCE 1938!"
+    elsif phrase = "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN"
+  else
+    "HUH?! NOT SINCE 1938"
+  end
+  
